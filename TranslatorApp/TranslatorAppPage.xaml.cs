b915539151ee223945d26811129a5772148a4638
JP Boro <jpcboro@gmail.com>
2017-06-20 23:20:26 +0800

    Blank Xamarin Forms iOS and PCL

---
+++ TranslatorApp/TranslatorAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TranslatorApp
+{
+    public partial class TranslatorAppPage : ContentPage
+    {
+        public TranslatorAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
